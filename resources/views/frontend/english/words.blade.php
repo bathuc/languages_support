ccb4fce017aa3c124016540bbd824d3834b09280
Thucle <ba.thuc@primelabo.com.vn>
2018-08-07 20:16:52 +0700

    modify code:

--- resources/views/frontend/english/words.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.frontend.index')
-
-@section('content')
-    <h1 id="head1">Words Drill</h1>
-    <input type="radio" name="words" value="1000_words" checked> First 1000 words
-    <input type="radio" name="words" value="Random"> Random
-@endsection
