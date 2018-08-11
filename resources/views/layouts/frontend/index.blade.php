d45862141ad711c620aac971ebc66674739d97a9
Thucle <lebathuc@gmail.com>
2018-08-11 15:43:10 +0700

    update code

--- resources/views/layouts/frontend/index.blade.php
+++ resources/views/layouts/frontend/index.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta content="IE=edge" http-equiv="X-UA-Compatible">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>My default App</title>
+		<title>Languages support</title>
         <meta name="description" content="">
         <link rel="shortcut icon" href="/favicon.ico?v=1">
 		<meta name="csrf-token" content="{{ csrf_token() }}">
